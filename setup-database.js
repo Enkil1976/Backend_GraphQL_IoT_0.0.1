68c00197c2d1589cdb40c552c0ba5727b3f39b0f
Tu Nombre <tu_email@ejemplo.com>
2025-07-06 01:24:54 -0400

    fix: correct de-init volumes and add log to setup-database.js

--- setup-database.js
+++ setup-database.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+console.log('Executing setup-database.js');
 const { pool } = require('./src/config/database');
 const fs = require('fs');
 const path = require('path');
